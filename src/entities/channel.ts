9312d7b0b1479aae715c157fe7da471ad3d3cc32
Mario Kranjec <mariokranjec91@gmail.com>
2020-04-24 17:50:43 +0200

    wip

--- src/entities/channel.ts
+++ src/entities/channel.ts
@@ -1,7 +1,7 @@
 import {BaseEntity, Column, Entity, PrimaryGeneratedColumn} from "typeorm";
 
 @Entity()
-export class Channel extends BaseEntity{
+export class Channel extends BaseEntity {
 
     @PrimaryGeneratedColumn()
     id: number;
