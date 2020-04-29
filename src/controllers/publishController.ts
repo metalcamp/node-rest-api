7924b444ccaf382db3c5aeed8e7623ccdce6e93d
Mario Kranjec <mariokranjec91@gmail.com>
2020-04-29 09:18:47 +0200

    wip

--- src/controllers/publishController.ts
+++ src/controllers/publishController.ts
@@ -1,7 +1,17 @@
 import {Request, Response} from "express";
+import redis from "../redis";
+import {Channel} from "../entities/Channel";
 
-export let publish = (req: Request, res: Response) => {
-    //TODO validation
-    console.log(req.body);
+export let publish = async (req: Request, res: Response) => {
+    const {channel: channelName} = req.params;
+
+    // TODO support glob pattern matching
+    let channel = await Channel.findOne({title: channelName});
+
+    if (channel === undefined) {
+        channel = await Channel.create({title: channelName}).save();
+    }
+
+    await redis.publish(channelName, req.body);
     res.status(201)
 };
