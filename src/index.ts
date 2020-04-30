2fd445e3a58b6d66feaa4d14e9afc9b064e4a25f
Mario Kranjec <mariokranjec91@gmail.com>
2020-04-30 13:58:39 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,19 @@ import {Server} from "./server";
 import {RSSWorker} from "./workers/RSSWorker";
 
 
-if ((process.env.ROLE === "server")) {
+if ((process.env.ROLE === "server") || (process.env.ROLE === undefined)) {
     console.log("running as server");
     const server = new Server();
     server.start();
 }
 
-if((process.env.ROLE === "publish-worker")){
+if ((process.env.ROLE === "publish-worker")) {
     console.log("running as publish worker")
     // TODO
 }
 
-if((process.env.ROLE === "rss-worker") || (process.env.ROLE === undefined)){
+if (process.env.ROLE === "rss-worker") {
     console.log("running as rss worker")
     const worker = new RSSWorker();
-    const res = worker.parseFromFile();
+    worker.start();
 }
--- src/workers/RSSWorker.ts
+++ src/workers/RSSWorker.ts
@@ -1,8 +1,10 @@
 import Parser from "rss-parser";
 import fs from "fs";
+import axios from "axios";
 
 export class RSSWorker {
-    private readonly url: string = "https://meteo.arso.gov.si/uploads/probase/www/observ/surface/text/sl/observation_si_latest.rss";
+    private readonly rssURL: string = "https://meteo.arso.gov.si/uploads/probase/www/observ/surface/text/sl/observation_si_latest.rss";
+    private readonly apiURL: string = "http://localhost:3000/api/v1/publish/";
     private parser: Parser;
 
     constructor() {
@@ -11,11 +13,11 @@ export class RSSWorker {
 
     async parseFromURL(url?: string) {
         try {
-            const rssURL = url ?? this.url;
+            const rssURL = url ?? this.rssURL;
             const feed = await this.parser.parseURL(rssURL);
             return feed.items;
         } catch (e) {
-            console.log(e);
+            console.error(e);
         }
     }
 
@@ -26,8 +28,25 @@ export class RSSWorker {
             const feed = await this.parser.parseString(data);
             return feed.items;
         } catch (e) {
-            console.log(e);
+            console.error(e);
         }
+    }
+
+    async publish(channel: string, message: object) {
+        try {
+            const url = this.apiURL + encodeURI(channel);
+            await axios.post(url, JSON.stringify(message));
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    async start() {
+        const rssItems = await this.parseFromFile();
+
+        rssItems.forEach((item) => {
+            this.publish(item.guid, item);
+        })
     }
 }
 
