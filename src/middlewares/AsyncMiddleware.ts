8a49f9c8088bf911a9ac0e01877001ca006c9c74
Mario Kranjec <mariokranjec91@gmail.com>
2020-05-06 10:19:51 +0200

    fix: remove console.log

--- src/middlewares/AsyncMiddleware.ts
+++ src/middlewares/AsyncMiddleware.ts
@@ -1,6 +1,5 @@
 import express from "express";
 
 export default (fn: express.RequestHandler) => (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log("promise");
     Promise.resolve(fn(req, res, next)).catch(next);
 };
