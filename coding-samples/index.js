7644a6dda4827fff7f27bcb8e8c101157a31e219
Mario Kranjec <mariokranjec91@gmail.com>
2020-05-06 08:14:09 +0200

    feat: add coding sample #2

--- coding-samples/index.js
+++ coding-samples/index.js
@@ -7,6 +7,35 @@
 const input1 = [1, 1, 1, 2, 3, 5, 6, 6]
 // console.log([...new Set(input1)]);
 
+//solution @2
+const promise1 = Promise.resolve("promise 1 resolved");
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        reject(new Error("promise 2 rejected"));
+    }, 1000)
+});
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('promise 3 resolved');
+    }, 2000)
+});
+
+Promise.all([
+    promise1.catch(error => {
+        return error;
+    }),
+    promise2.catch(error => {
+        return error;
+    }),
+    promise3.catch(error => {
+        return error;
+    }),
+]).then(values => {
+    console.log(values[0]);
+    console.log(values[1]);
+    console.log(values[2]);
+});
+
 // solution 1 @4
 const input4 = [{value: 1, id: "test1"}, {value: 2, id: "test2"}, {value: 3, id: "test3"}]
 const reducer = (accumulator, currentValue) => {
