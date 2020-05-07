21e13602cb19996d8036fd3b8cb39f545e677cb2
Mario Kranjec <mariokranjec91@gmail.com>
2020-05-07 13:49:23 +0200

    feat: setup jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
+    },
+    testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    verbose: true,
+    collectCoverage: true,
+    testPathIgnorePatterns: ["/dist/", "/node_modules/"],
+};
