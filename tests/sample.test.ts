16d1612977caa381a979613c0d953a9b86ae0df0
Mario Kranjec <mariokranjec91@gmail.com>
2020-05-07 13:51:16 +0200

    tests: sample tests

--- tests/sample.test.ts
+++ tests/sample.test.ts
@@ -1,5 +1,25 @@
+const constant: number = 1
+
+const asyncFunction = async () => {
+    return Promise.resolve(1);
+}
+
 describe("This is a simple test", () => {
-    test("Check the sampleFunction function", () => {
-        expect(1).toEqual(1);
+    test("Check that constant is 1", () => {
+        expect(constant).toEqual(1);
+    });
+});
+
+describe("This is a simple promise test", () => {
+    test("Check that promise resolves",  () => {
+        const p = Promise.resolve(1);
+        expect(p).resolves.toEqual(1)
+    });
+});
+
+describe("This is a simple async test", () => {
+    test("Check that async function returns expected value",  async() => {
+        const p = await asyncFunction();
+        expect(p).toEqual(1)
     });
 });
