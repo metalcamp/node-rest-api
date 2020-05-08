4e9eb55f8df0c728835a9b78a2a74b62b8067bb2
Mario Kranjec <mariokranjec91@gmail.com>
2020-05-08 10:31:48 +0200

    tests: refactor integration tests

--- tests/integration/status.test.ts
+++ tests/integration/status.test.ts
@@ -1,18 +1,18 @@
 // tslint:disable-next-line:no-implicit-dependencies
 import request from "supertest";
-import {App} from '../../src/app';
+import app from "../utils/setupApp";
 
 describe("This is a simple status test", () => {
-    test("Check that status route exists and returns 200",  () => {
-        const app = (new App()).getServer();
-        const res = request(app)
+    test("Check that status route exists and returns 200",async () => {
+        const myApp = await app;
+        const res = request(myApp)
             .get('/api/v1/status')
             .expect(200)
     });
 
-    test("Check some random route does not exist and returns 404",  () => {
-        const app = (new App()).getServer();
-        const res = request(app)
+    test("Check some random route does not exist and returns 404", async () => {
+        const myApp = await app;
+        const res = await request(myApp)
             .get('/api/v1/status/232394')
             .expect(404)
     });
